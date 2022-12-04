bead08dab19594c180b1b05b2335a8790447d385
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-04 16:22:07 +0100

    Discussions Some Styling

--- app/src/main/java/promag/groupe/proapp/models/Discussion.kt
+++ app/src/main/java/promag/groupe/proapp/models/Discussion.kt
@@ -1,11 +1,17 @@
 package promag.groupe.proapp.models
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import com.google.gson.annotations.SerializedName
+import promag.groupe.proapp.ISO_DATE_TIME_FORMAT
 import promag.groupe.proapp.utils.difference
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class Discussion(
@@ -45,8 +51,8 @@ class Discussion(
 
     val shortMessage: String
         get() {
-            if (lastMessage == null || lastMessage!!.message.isEmpty()) return "Discussion Vide"
-            val message = lastMessage?.message ?: return "Discussion Vide"
+            if (lastMessage == null || lastMessage!!.message.isEmpty()) return "Nouvelle Discussion"
+            val message = lastMessage?.message ?: return "Nouvelle Discussion"
             if (message.length > 70) return "${message.substring(0, 70)} ..."
             return message
 
@@ -55,16 +61,39 @@ class Discussion(
     val fromTime: String
         @RequiresApi(Build.VERSION_CODES.O)
         get() {
-//            if (lastMessage == null || lastMessage!!.message.isEmpty()) return ""
-//            val dateCreation = lastMessage!!.dateCreation
-//            if (dateCreation.isNullOrEmpty()) return ""
-////            val df: DateFormat = SimpleDateFormat(ISO_DATE_TIME_FORMAT)
+            if (lastMessage == null || lastMessage!!.message.isEmpty()) return ""
+            var dateCreation = lastMessage!!.dateCreation
+            if (dateCreation.isNullOrEmpty()) return ""
+//            dateCreation = dateCreation.replace("T", " ")
+
+            Log.d("DATE DIFF", "$this - $dateCreation: ")
+
+
+
+            try {
+//                val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.ssssss'Z'", Locale.FRANCE)
+                val format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
+                val date = LocalDateTime.parse(dateCreation, format);
+//                val date = format.parse(dateCreation)
+                val now = LocalDateTime.now()
+
+                Log.d("DATE DIFF", "$now - $date: ")
+
+                return now.difference(date)
+
+            } catch (e: ParseException) {
+                // TODO Auto-generated catch block
+                e.printStackTrace()
+                return "dateTime"
+
+            }
+//            val df: DateFormat = SimpleDateFormat(ISO_DATE_TIME_FORMAT)
 //            val ldt: LocalDateTime = LocalDateTime.parse(dateCreation)
 //
 //            val date = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
 //            val now = Date()
 //            return now.difference(date)
-            return "24 minute(s)"
+//            return "24 minute(s)"
 
         }
 }
--- app/src/main/java/promag/groupe/proapp/utils/Operations.kt
+++ app/src/main/java/promag/groupe/proapp/utils/Operations.kt
@@ -1,13 +1,54 @@
 package promag.groupe.proapp.utils
 
-import java.util.Date
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.*
+import kotlin.math.abs
 
-fun Date.difference(date: Date) : String{
-    val diff: Long = this.time - date.time
-    val seconds = diff / 1000
+//https://www.behance.net/gallery/86147197/Whatsapp-Redesign-Concept?tracking_source=search_projects%7Cwhatsapp
+
+fun Date.difference(date: Date): String {
+
+    Log.d("DATE DIFF", "$this - $date: ")
+    val timeDiff: Long = abs(this.time - date.time)
+    val seconds = timeDiff / 1000
+
+    if (seconds < 60)
+        return "$seconds second(s)"
     val minutes = seconds / 60
+    if (minutes < 60)
+        return "$minutes minute(s)"
     val hours = minutes / 60
+
+    if (hours < 24)
+        return "$hours heure(s)"
+
     val days = hours / 24
 
-    return "$minutes minutes, "
+    return "$days jour(s)"
+}
+@RequiresApi(Build.VERSION_CODES.O)
+fun LocalDateTime.difference(date: LocalDateTime): String {
+
+    Log.d("DATE DIFF", "$this - $date: ")
+    val seconds: Long = abs(this.toEpochSecond(ZoneOffset.UTC) - date.toEpochSecond(ZoneOffset.UTC))
+//    val seconds = timeDiff / 1000
+
+    if (seconds < 60)
+        return "$seconds second(s)"
+    val minutes = seconds / 60
+    if (minutes < 60)
+        return "$minutes minute(s)"
+    val hours = minutes / 60
+
+    if (hours < 24)
+        return "$hours heure(s)"
+
+    val days = hours / 24
+
+    return "$days jour(s)"
 }
