e1f193879e8e8a6b65defd2d4b6de2dc7fcfab55
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-05 18:09:56 +0100

    Local config

--- app/src/main/java/promag/groupe/proapp/Constants.kt
+++ app/src/main/java/promag/groupe/proapp/Constants.kt
@@ -1,8 +1,8 @@
 package promag.groupe.proapp
 
 const val NOTIFICATION_END = "https://eassalnotif.herokuapp.com/"
-const val BASE_URL = "http://10.0.0.20:8000/"
-//const val BASE_URL = "http://192.168.1.38:8000/"
+//const val BASE_URL = "http://10.0.0.20:8000/"
+const val BASE_URL = "http://192.168.1.38:8000/"
 const val BASE_URL_API = BASE_URL + "api/"
 const val USER_PREFERENCE = "user_data"
 const val USER_TOKEN = "token"
