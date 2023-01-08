fc376c337295eb1dd317cd6499c7ccc3e7476c70
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-08 16:23:48 +0100

    Add Permission Alarm

---
+++ app/src/main/java/promag/groupe/proapp/services/AlarmSample.kt
@@ -0,0 +1,10 @@
+package promag.groupe.proapp.services
+
+import android.app.AlarmManager
+import android.content.Context
+import androidx.core.content.ContextCompat.getSystemService
+
+class AlarmSample {
+
+//    private val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+}
