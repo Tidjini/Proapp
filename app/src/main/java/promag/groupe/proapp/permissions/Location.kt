5a9ab43757888bbd4ee7a36457d84328f2af3042
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-10 09:59:29 +0100

    Location Permission - check permission location for both fine location and coarse location

--- app/src/main/java/promag/groupe/proapp/permissions/Location.kt
+++ app/src/main/java/promag/groupe/proapp/permissions/Location.kt
@@ -1,4 +1,32 @@
 package promag.groupe.proapp.permissions
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+
+//const val ACCESS_FINE_LOCATON = "android.permission.ACCESS_FINE_LOCATION"
+//const val ACCESS_COARSE_LOCATON = "android.permission.ACCESS_COARSE_LOCATION"
+const val LOCATION_PERMISSION_REQUEST_CODE = 2000
 class Location {
+
+    companion object{
+        fun checkPermissionLocation(context: Context): Boolean {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+
+            }
+            return true
+        }
+    }
+
+
 }
