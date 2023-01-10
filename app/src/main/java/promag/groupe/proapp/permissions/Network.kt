59355104f1b0ef9566809fc426a781db0ac3771d
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-10 13:55:21 +0100

    Check Network state

---
+++ app/src/main/java/promag/groupe/proapp/permissions/Network.kt
@@ -0,0 +1,44 @@
+package promag.groupe.proapp.permissions
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import promag.groupe.proapp.BaseApplication
+
+class Network {
+
+    companion object {
+        fun checkNetworkState(application: BaseApplication): Boolean {
+            var result = false
+            val connectivityManager =
+                application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val networkCapabilities = connectivityManager.activeNetwork ?: return false
+                val actNw =
+                    connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+                result = when {
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } else {
+                connectivityManager.run {
+                    connectivityManager.activeNetworkInfo?.run {
+                        result = when (type) {
+                            ConnectivityManager.TYPE_WIFI -> true
+                            ConnectivityManager.TYPE_MOBILE -> true
+                            ConnectivityManager.TYPE_ETHERNET -> true
+                            else -> false
+                        }
+
+                    }
+                }
+            }
+
+            return result
+        }
+
+    }
+}
