2be3df600fa3cfdadbe2a6eb663a71657d74fa42
Tidjini <tidjini.messaoudi@gmail.com>
2023-01-10 10:00:30 +0100

    clean helpers from check permission location function

--- app/src/main/java/promag/groupe/proapp/utils/Helpers.kt
+++ app/src/main/java/promag/groupe/proapp/utils/Helpers.kt
@@ -83,20 +83,6 @@ class Helpers {
             }
         }
 
-        fun checkFineLocationPermission(context: Context): Boolean {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return false
-
-            }
-            return true
-        }
     }
 
 }
